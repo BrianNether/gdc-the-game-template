7ea90c1d066d12d89f8b49959d3571fbc6e210fa
BrianNether <66758302+BrianNether@users.noreply.github.com>
2026-09-06 18:59:34 -0400

    Fixed Build Your Schedule not hiding cursor

--- micro_games/build_your_schedule/scripts/cursor.gd
+++ micro_games/build_your_schedule/scripts/cursor.gd
@@ -3,9 +3,14 @@ class_name BuildScheduleCursor
 
 var prior_mouse_mode: int
 
+func get_empty_texture() -> Texture:
+	return preload("res://micro_games/build_your_schedule/empty.png")
+
 func _ready() -> void:
 	prior_mouse_mode = Input.get_mouse_mode()
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	Input.set_custom_mouse_cursor(get_empty_texture(), Input.CURSOR_ARROW)
+	Input.set_custom_mouse_cursor(get_empty_texture(), Input.CURSOR_POINTING_HAND)
 	
 func _process(_delta: float) -> void:
 	global_position = get_global_mouse_position()
